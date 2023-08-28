0b569a3edf4ecebc13cc1e7b21fa1e1b6321034a
Jetro Tull Roa <jetrotullroa@gmail.com>
2023-08-28 06:53:29 -0400

    install and test bootstrap

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
-
-
-import styles from '@/pages/index.module.css'
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <div>THIS IS A PAGE</div>
-  )
+    <Container
+      className="d-flex flex-column align-items-center justify-content-center vh-100 w-100"
+      fluid
+    >
+      <Row>
+        <Col>QUESTIONS GOES HERE...</Col>
+      </Row>
+      <Row>
+        <Col>SELECTION GOES HERE</Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button>Back</Button>
+        </Col>
+        <Col>
+          <Button>Next</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
