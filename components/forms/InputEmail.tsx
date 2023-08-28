4563a3432527bb78819f676d8cb0cfafc88e0170
Jetro Tull Roa <jetrotullroa@gmail.com>
2023-08-28 10:12:00 -0400

    form validation

---
+++ components/forms/InputEmail.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { Form } from 'react-bootstrap';
+import { FormProps } from 'types/Form';
+import { validateMin, validateMax, validatePresent, validateLettersOnly, validateEmail } from 'utils/validation';
+import { ValidationMessage } from '../common/ValidationError';
+
+export function InputEmail({
+  id,
+  type,
+  placeholder,
+  minimumRequired,
+  maximumRequired,
+	required
+}: FormProps) {
+  const firstRender = useRef(true);
+  const [value, setValue] = useState('');
+
+  const validationMessages = useMemo(() => {
+    return [
+			validateEmail(value),
+      validateMin(value, minimumRequired),
+      validateMax(value, maximumRequired)
+    ].filter(Boolean);
+  }, [value]);
+
+	const isNotValid = required && validationMessages.length !== 0
+
+
+  useEffect(() => {
+    firstRender.current = false;
+  }, [value]);
+
+  return (
+    <>
+      <Form.Control
+        placeholder={placeholder}
+        size="lg"
+        type={type}
+        id={`question-${id}`}
+        aria-describedby={`${id}HelpBlock`}
+        autoFocus
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      {!firstRender.current && isNotValid && (
+        <ValidationMessage message={validationMessages[0]} />
+      )}
+    </>
+  );
+}
---
+++ components/forms/InputNumber.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { Form } from 'react-bootstrap';
+import { FormProps } from 'types/Form';
+import { validateMaxNumber, validatePresent, validateMinNumber } from 'utils/validation';
+import { ValidationMessage } from '../common/ValidationError';
+import { randomInt } from 'utils/common';
+
+export function InputNumber({ id, type, placeholder, required, maximumRequired, minimumRequired }: FormProps) {
+  const firstRender = useRef(true);
+  const [value, setValue] = useState(randomInt(minimumRequired, maximumRequired));
+
+  const validationMessages = useMemo(() => {
+    return [
+      validateMinNumber(value, minimumRequired),
+      validateMaxNumber(value, maximumRequired)
+    ].filter(Boolean);
+  }, [value]);
+
+	const isNotValid = required && validationMessages.length !== 0
+
+  useEffect(() => {
+		if (firstRender.current && value > 999) {
+			setValue(999)
+		}
+		if (firstRender.current && value < 0) {
+			setValue(0)
+		}
+    firstRender.current = false;
+  }, [value]);
+
+  return (
+    <>
+      <Form.Control
+        placeholder={placeholder}
+        size="lg"
+        type={type}
+        min={0}
+        max={999}
+        id={`question-${id}`}
+        aria-describedby={`${id}HelpBlock`}
+        autoFocus
+        onChange={(e) => setValue(parseInt(e.target.value, 10))}
+				value={value}
+      />
+      {!firstRender.current && isNotValid && (
+        <ValidationMessage message={validationMessages[0]} />
+      )}
+    </>
+  );
+}
